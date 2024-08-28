eca66569745c4ee0c49a40a45202ed049becb37a
Duckdey <minhduck1726@gmail.com>
2024-08-28 16:02:59 +0700

    add ex37

---
+++ ex37/CharArr.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    char c[] = "abcd";
+    printf("Size of c = %d", sizeof c);
+}
+// luôn có 1 size cho kí tự "\0"
---
+++ ex37/ScanfVsPrintf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    char str[10];
+    scanf("%s", str);  //s=string (chuỗi)
+    printf("Chuoi vua nhap la: %s", str);
+}
